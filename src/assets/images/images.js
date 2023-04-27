30b4222c6a1e2bdc702f45897dd23bbfc7f347a5
Sourav <Sauravpandeyaz897@gmail.com>
2023-04-27 15:45:45 +0530

    First commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,27 @@
+import wallpaper_main from "./wallpaper_main.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jpeg";
+import artists from "./artists.jpeg";
+import albums from "./Hostel-1.jpg";
+import song1Img from "./kishori.jpg";
+import song2Img from "./pop-smoke.jpg";
+
+const images = {
+  wallpaper_main,
+  wallpaper2,
+  wallpaper3,
+  wallpaper4,
+  wallpaper5,
+  coverflow,
+  games,
+  artists,
+  albums,
+  song1Img,
+  song2Img,
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,9 @@
+import music1 from "./Kishori.mp3";
+import music2 from "./Invincible.mp3";
+
+const songs = {
+  music1,
+  music2,
+};
+
+export default songs;
